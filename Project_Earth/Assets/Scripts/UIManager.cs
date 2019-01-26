c70ebef3efb099d72725bed205e6718de1df57fe
Aidan Brown <Abrownie99600@gmail.com>
2019-01-26 02:18:15 -0500

    Started the game UI

---
+++ Project_Earth/Assets/Scripts/UIManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    public Slider hpBar;
+    private int totalHP = 100;
+    public GameObject earth, tPanel, sPanel;
+
+    public void Update()
+    {
+        totalHP--;
+        hpBar.value = 100 - totalHP;
+    }
+
+    public void ToggleTPanel()
+    {
+        if(sPanel.active == false)
+            tPanel.SetActive(!tPanel.active);
+    }
+
+    public void ToggleSPanel()
+    {
+        if(tPanel.active == false)
+            sPanel.SetActive(!sPanel.active);
+    }
+}
