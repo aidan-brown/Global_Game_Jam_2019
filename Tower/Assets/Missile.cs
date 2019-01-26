18085135ffdbd1494a23a549aa5487a8580cc611
River Marks <rmarks6767@gmail.com>
2019-01-26 15:56:30 -0500

    Created the rocket tower stuff

---
+++ Tower/Assets/Missile.cs
@@ -0,0 +1,49 @@
+﻿/**Code referenced from 
+ * Wabble Unity Tutorials 
+ * on Youtube
+*/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Missile : MonoBehaviour
+{
+    public float speed = 3;
+    public float rotatingSpeed = 200;
+    public GameObject target;
+
+    Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 pointToTarget = (Vector2)transform.position - (Vector2)target.transform.position;
+
+        pointToTarget.Normalize();
+        float value = Vector3.Cross(pointToTarget, transform.right).z;
+
+        if (value > 0)
+        {
+            rb.angularVelocity = rotatingSpeed;
+        }
+        else if(value < 0)
+        {
+            rb.angularVelocity = -rotatingSpeed;
+        }
+        else
+        {
+            rotatingSpeed = 0;
+        }
+        rb.velocity = transform.right * speed;
+    }
+
+    public void SetTarget(GameObject target)
+    {
+        this.target = target;
+        Debug.Log("The target was set");
+    }
+}
