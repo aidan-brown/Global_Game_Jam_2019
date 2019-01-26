cd2613e31a1525923f167029c9be587db2c0d29d
Lauren Bullock <laurencbullock@gmail.com>
2019-01-26 01:22:01 -0500

    Earth Setup

---
+++ Project_Earth/Assets/Scripts/Earth/Earth.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Earth : MonoBehaviour
+{
+	[SerializeField] int RotationSpeed;
+    [SerializeField] GameObject tile; // tile to create
+    [SerializeField] int tileAmount;
+    [SerializeField] List<GameObject> spaces;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector3 center = transform.position;
+        for (int i = 0; i < tileAmount; i++)
+        {
+            Vector3 pos = placeOnCircle(center, 2.2f, i*20);
+            Quaternion rot = Quaternion.FromToRotation(Vector3.right, center - pos);
+            GameObject temp = Instantiate(tile, pos, rot);
+            temp.transform.parent = gameObject.transform;
+            spaces.Add(temp);
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		//rotate earth
+		transform.Rotate ( Vector3.forward* ( RotationSpeed * Time.deltaTime ) );
+    }
+
+
+    Vector3 placeOnCircle(Vector3 center, float radius, int angle)
+    {
+        float ang = angle;
+        Vector3 pos;
+        pos.x = center.x + radius * Mathf.Sin(ang * Mathf.Deg2Rad);
+        pos.y = center.y + radius * Mathf.Cos(ang * Mathf.Deg2Rad);
+        pos.z = center.z;
+        return pos;
+    }
+
+    public void showTiles()
+    {
+        foreach (GameObject t in spaces)
+        {
+            t.GetComponent<Tile>().showTile();
+
+        }
+    }
+
+    public void hideTiles()
+    {
+        foreach (GameObject t in spaces)
+        {
+            t.GetComponent<Tile>().hideTile();
+
+        }
+    }
+}
+
+
+
---
+++ Project_Earth/Assets/Scripts/Earth/Tile.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    GameObject containedTower = null;
+    bool canPlace = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void addTower(GameObject tower)
+    {
+        Quaternion rot = Quaternion.FromToRotation(Vector3.right, transform.position);
+        GameObject temp = Instantiate(tower, transform.position, rot);
+        temp.transform.parent = gameObject.transform;
+        containedTower = temp;
+        canPlace = false;
+        hideTile();
+    }
+
+    void removeTower()
+    {
+
+        //if we make a list of towers it must be removed as well
+        Destroy(gameObject.transform.GetChild(0).transform.gameObject);
+        containedTower = null;
+        canPlace = true;
+    }
+
+    public void showTile()
+    {
+        if (canPlace == true)
+        {
+            gameObject.GetComponent<BoxCollider2D>().enabled = true;
+            Color tmp = Color.red;
+            tmp.a = 0.5f;
+            gameObject.GetComponent<SpriteRenderer>().color = tmp;
+        }
+      
+    }
+
+    public void hideTile()
+    {
+        Color tmp = Color.red;
+        tmp.a = 0.0f;
+        gameObject.GetComponent<SpriteRenderer>().color = tmp;
+        gameObject.GetComponent<BoxCollider2D>().enabled = false;
+    }
+
+    //handles adding Towers
+    void OnMouseDown()
+    {
+        if (ItemHolder.instance.HeldItem != null && canPlace == true)
+        {
+            addTower(ItemHolder.instance.HeldItem);
+        }
+        else if (canPlace == false)
+        {
+            removeTower();
+        }
+    }
+}
