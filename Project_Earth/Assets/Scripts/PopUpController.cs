df5c60a3bd490842f0d841111fafa4dd89dbd069
Aidan Brown <Abrownie99600@gmail.com>
2019-01-26 17:05:55 -0500

    Added pop up panels w/ content

---
+++ Project_Earth/Assets/Scripts/PopUpController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PopUpController : MonoBehaviour
+{
+    public GameObject tower;
+    public Text dpsText, frText, rText, thpText;
+    public Slider hpBar;
+
+    public void Close()
+    {
+        Destroy(this.gameObject);
+    }
+
+    private void Update()
+    {
+        
+    }
+}
